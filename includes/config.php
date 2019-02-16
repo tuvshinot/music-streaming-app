9c61d0e419d432307d0e7b616b83504bb5299bd3
tuvshin <tuvshin.otgo@gmail.com>
2019-02-16 20:15:45 +0300

    changed db name to slotify

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
     session_start();
 
     $timezone = date_default_timezone_set("Europe/Moscow");
-    $con = mysqli_connect("localhost", "root", "", "spotify");
+    $con = mysqli_connect("localhost", "root", "", "slotify");
 
     if(mysqli_connect_errno()) { // error when connecting to mysql
         echo "Failed to connect: " . mysqli_connect_errno();
